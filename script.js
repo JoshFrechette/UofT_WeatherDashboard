342c37fe8e233b73b2b1c58839be56f04d3c6299
Josh Frechette <josh.f.design@gmail.com>
2019-10-31 23:13:01 -0400

    Successfully calling openweatherAPI, formatting DOMS and proper button functions

--- script.js
+++ script.js
@@ -1,53 +1,72 @@
-var today = moment().subtract(10, 'days').calendar(); 
+//Fetch the date from moment.js in the proper form
+var today = moment().format('DD/MM/YYYY');//not right
 console.log(today + " is the day that the user is seeing for the current forecast")
-
+//An empty array to hold the city search history
 var cities =[];
 
-function displayWeatherInfo() {
-
-  var city = $(this).attr("city-name");
+//The function that will 
+function displayWeatherInfo(city) {
   
-  //var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=4df9a8b74a9e64205192fa481c567a1b";
-  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "=4df9a8b74a9e64205192fa481c567a1b";
-    console.log(queryURL + " is the OpenWeather City response")
+
+  //Get the city name from the text input box
+  //var city = $(this).text();// Changed from "city-name"
+  //Get the current weather 
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=4df9a8b74a9e64205192fa481c567a1b";
+    console.log(queryURL + " is the OpenWeather City response for today's weather")
+  //var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=4df9a8b74a9e64205192fa481c567a1b";
+  //console.log(queryURL2 + " is the OpenWeather City response for 5 day forecast")
     console.log(city)
 
+// Add code for 5 day forecast once current weather is working
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        var cityName = $("#current").text(response.city + today);
-        console.log(cityName + " is the name that appears in the current weather field")
+        console.log("hi");
+        console.log($("#current").text(response.name + " (" + today + ")"));
+        
+        console.log(city + " is the name that appears in the current weather field")
 
-        $("#current").empty();
-        $("#current").append(cityName);
+        $("#current").empty();// Clear previous search to avoid repeats
+
+        $("#current-weather").append("<h2>").text(response.name + " (" + today + ")");
+        $("#current-temp").append("<p>").text("Temperature: " + response.main.temp);
+        console.log(response.main.temp + "is the temperature right now")
+        $("#current-humid").append("<p>").text("Temperature: " + response.temp);
+        $("#current-windspd").append("<p>").text("Temperature: " + response.temp);
+        $("#current-UV").append("<p>").text("Temperature: " + response.temp);
+
+        queryURL = "hi";
+        /*$.ajax({
+            url: queryURL2,
+            method:"GET"
+        }).then(function(response){
+            console.log(response)
+            $("#forecast").empty();// Clear previous search to avoid repeats
+            $("#day2").text();
+
+
+        });*/
 
     });
     console.log(city)
 }
-
+//Make the city search history buttons
 function renderButtons(){
-
+    //Empty previous search
     $("#search-history").empty();
 
     for (var i = 0; i < cities.length; i++) {
 
-        $("#search-history").append($("<button>" + cities[i] + "</button>").addClass("past-city"));
-
-       /*var a = $("<button>");
-
-        a.addClass("past-city");
-
-        a.attr("city-name", cities[i]);
-
-        a.text(cities[i]);
-*/
-        
+        $("#search-history").append($("<button>").text(cities[i]).addClass("past-city").on("click", function(){
+            displayWeatherInfo(cities[i]);
+        }));     
     }
 }
 
-$("#add-city").on("click", function(event) {
+$("#add-city").on("click", function(event){
     event.preventDefault();
+    //newFunction();
 
     var city = $("#city-input").val().trim();
     console.log(city)
@@ -55,11 +74,17 @@ $("#add-city").on("click", function(event) {
 
     cities.push(city);
     console.log(cities + " updated cities array")
+    displayWeatherInfo(city);
 
     renderButtons();
 });
 
-$(document).on("click", ".past-city", displayWeatherInfo);
+// $(".past-city").on("click", displayWeatherInfo);
 console.log(cities)
 
 renderButtons();
+function newFunction() {
+    console.log("new function");
+    displayWeatherInfo();
+}
+
