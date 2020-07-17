156b8fbb668896d72a0bce4ecfbc1a06c057155b
Josh Frechette <josh.f.design@gmail.com>
2020-07-16 21:33:58 -0400

    Fixed Search field.

--- script.js
+++ script.js
@@ -13,11 +13,12 @@ function displayWeatherInfo(city) {//Display all weather info
     $("#current").empty();
     $(".five-day").empty();
     $(".forecast-head").empty();
-    
+    $("#city-input").empty();
+
     console.log(city)
 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + weatherKey;//Get the current weather 
-    console.log(queryURL + " is the OpenWeather City response for today's weather")
+    // console.log(queryURL + " is the OpenWeather City response for today's weather")
 
     $.ajax({
         url: queryURL,
@@ -25,7 +26,7 @@ function displayWeatherInfo(city) {//Display all weather info
     }).then(function (response, lat, long) { //change the function name to diif. it from the other functions (UV Index, forecast, etc..)?
         var lat = response.coord.lat;
         var long = response.coord.lon;
-        console.log(city + long + " and " + lat)
+        // console.log(city + long + " and " + lat)
         var cityTemp = Math.round(response.main.temp);
         var cityTempIcon = response.weather[0].icon;
         var cityTempIconURL = "http://openweathermap.org/img/w/" + cityTempIcon + ".png";
@@ -34,43 +35,43 @@ function displayWeatherInfo(city) {//Display all weather info
         $("#current").empty();//
         var CityLat = lat;
         var CityLong = long;
-        console.log(city + CityLong + " and " + CityLat)
+        // console.log(city + CityLong + " and " + CityLat)
 
         //==================== API call for Longitude and latitude for UV Index Info =====================
-            var queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + CityLat + "&lon=" + CityLong;
-            console.log(queryURL + " is the OpenWeather City response for the uv index")
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
-                console.log(response)
-                $("#current")
-                    .append($("<h2>").html(city + " (" + today + ")" + "<img src=" + cityTempIconURL + ">")) //add the city name, weather icon and date to the curent weather display
-                    .append($("<p>").html("Temperature: " + cityTemp + " °F")) //add the temperature
-                    .append($("<p>").html("Humidity: " + cityHumidity + "%"))//add the humidity
-                    .append($("<p>").html("Windspeed: " + cityWindSpeed + " MPH"))//add the windspeed
-                    .append($("<p>").html("<p>UV Index: <span id = current-UV><nbsp>" + response.value + "<nbsp></span></p>"))
-            });
+        var queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + CityLat + "&lon=" + CityLong;
+        // console.log(queryURL + " is the OpenWeather City response for the uv index")
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            // console.log(response)
+            $("#current")
+                .append($("<h2>").html(city + " (" + today + ")" + "<img src=" + cityTempIconURL + ">")) //add the city name, weather icon and date to the curent weather display
+                .append($("<p>").html("Temperature: " + cityTemp + " °F")) //add the temperature
+                .append($("<p>").html("Humidity: " + cityHumidity + "%"))//add the humidity
+                .append($("<p>").html("Windspeed: " + cityWindSpeed + " MPH"))//add the windspeed
+                .append($("<p>").html("<p>UV Index: <span id = current-UV><nbsp>" + response.value + "<nbsp></span></p>"))
+        });
     });
 
 
     //========================================== 5 Day Forecast ======================================
 
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + weatherKey;
-    console.log(queryURL + " is the OpenWeather City response for 5 day forecast")
+    // console.log(queryURL + " is the OpenWeather City response for 5 day forecast")
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response)
+        // console.log(response)
         $(".five-day").empty();//
         $(".forecast-head").empty();//
         $(".forecast-head")
 
-            .append($("<h2>").text("5-Day Forecast:"))
+        .append($("<h2>").text("5-Day Forecast:"))
         for (i = 0; i <= 4; i++) {
-            console.log(i)
+            // console.log(i)
             var nextDay = moment().add(1 + i, 'days').format('DD/MM/YYYY');
             var cityIconForecast = response.list[i].weather[0].icon;
             var cityIconURLForecast = "http://openweathermap.org/img/w/" + cityIconForecast + ".png";
@@ -82,8 +83,8 @@ function displayWeatherInfo(city) {//Display all weather info
                     .append($("<img src=" + cityIconURLForecast + ">")) //add the weather icon
                     .append($("<p>").html("Temp: " + cityTempForecast + " °F"))//add the temperature
                     .append($("<p>").html("Humidity: " + cityHumidityForecast + "%")))//add the humidity
-            console.log(nextDay + ", " + cityHumidityForecast + ", " + cityTempForecast)
-       
+            // console.log(nextDay + ", " + cityHumidityForecast + ", " + cityTempForecast)
+
         }
     });
 
@@ -93,16 +94,16 @@ function displayWeatherInfo(city) {//Display all weather info
 //Make the city search history buttons
 function renderButtons() {
     var cityInitial = $("#city-input").val().trim();
-    var citySearch=cityInitial.charAt(0).toUpperCase() + cityInitial.substring(1);//Sets the the first character as a capital
-        if (cities.indexOf($("#city-input").val().trim()) === -1) {// Avoids repeat search history buttons
-                 $("#search-history").append($("<button>").addClass("past-city").attr("city-name",citySearch ).text(citySearch))
-                 cities.push(citySearch);
-        }
+    var citySearch = cityInitial.charAt(0).toUpperCase() + cityInitial.substring(1);//Sets the the first character as a capital
+    if (cities.indexOf($("#city-input").val().trim()) === -1) {// Avoids repeat search history buttons
+        $("#search-history").append($("<button>").addClass("past-city").attr("city-name", citySearch).text(citySearch))
+        cities.push(citySearch);
+    }
 
     $(".past-city").on("click", function () { //Button click function to advance to 
-        console.log($(this).attr("city-name"))       
+        // console.log($(this).attr("city-name"))
         displayWeatherInfo($(this).attr("city-name"));
-    }) 
+    })
 }
 
 $("#add-city").on("click", function (event) {
@@ -116,11 +117,11 @@ $("#add-city").on("click", function (event) {
     // clearField(){
     //     document.getElementById("city-input").value = "";
     // }
-     
+
 });
 
 function newFunction() {
-    console.log("new function");
+    // console.log("new function");
     displayWeatherInfo();
 }
 
